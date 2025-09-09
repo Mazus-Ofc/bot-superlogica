7a7066676ec545c21a87f92d353a8a1ff7dd2d6c
Mazus <135450725+Mazus-Ofc@users.noreply.github.com>
2025-09-09 13:53:50 -0300

    Update wa.js

--- src/wa.js
+++ src/wa.js
@@ -1,17 +1,28 @@
 // src/wa.js
+"use strict";
+
 const wppconnect = require("@wppconnect-team/wppconnect");
+
 const sessions = new Map();
 
-async function getClient(sessionName = "default") {
+async function getClient(sessionName = "whats-default") {
   if (sessions.has(sessionName)) return sessions.get(sessionName);
+
   const client = await wppconnect.create({
     session: sessionName,
     headless: true,
-    puppeteerOptions: { args: ["--no-sandbox", "--disable-setuid-sandbox"] },
     protocolTimeout: 120000,
+    puppeteerOptions: { args: ["--no-sandbox", "--disable-setuid-sandbox"] },
+    catchQR: (base64Qr, asciiQR, attempts) => {
+      console.log(`[${sessionName}] QR attempts:`, attempts);
+    },
+    statusFind: (statusSession, session) => {
+      console.log(`[${sessionName}] status:`, statusSession);
+    },
   });
+
   sessions.set(sessionName, client);
   return client;
 }
 
-module.exports = getClient; // << exporta a função diretamente
+module.exports = { getClient }; // <<< export NOMEADO
